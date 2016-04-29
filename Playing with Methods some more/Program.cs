5e12f85c23d3c52d133f43869d6b7f1545bad53b
adoni <adoni@MSI>
2016-04-29 14:28:39 -0400

    Add project files.

---
+++ Playing with Methods some more/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playing_with_Methods_some_more
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
